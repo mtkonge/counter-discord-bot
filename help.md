5467b80697be561250e3eeef9378ea21ea043543
Mikkel Troels Kongsted <mtkongsted@gmail.com>
2023-09-29 21:26:04 +0200

    python match case my beloved -piet

---
+++ help.md
@@ -0,0 +1,11 @@
+Uh oh you frickin' moron, looks like you fucked up!
+ICBMs are being sent to your address as we speak.
+Next time, try:
+```
+- view stats for server
+    @Pushups Counter stats
+- view stats for N amount of users
+    @Pushups Counter stats @User0 ... @UserN
+- log your pushups
+    @Pushups Counter log 1337
+```
--- main.py
+++ main.py
@@ -17,45 +17,57 @@ def error_embed(name, value):
     embed.add_field(name=name, value=value)
     return embed
 
-def pushups_embed(users_pushups):
+def help_embed():
+    with open("help.md") as file:
+        embed = discord.Embed(title='Help', colour = discord.Colour.blue(), )
+        embed.add_field(name=" ", value=file.read(), inline=True)
+        return embed
+
+def pushups_embed(users_pushups: dict):
     embed = discord.Embed(title='Pushups', colour = discord.Colour.blue())
     for user_id, pushups in users_pushups.items():
         user = client.get_user(int(user_id))
-
         embed.add_field(name=user.display_name, value=pushups, inline=False)
     return embed
 
 
-def get_pushup_data_from_user(user_id: int):
-    file = open(JSON_FILE, "r") 
+def get_pushup_data_from_user(user_id: int) -> int:
+    file = open(JSON_FILE, "r")  
     pushups_data = json.load(file)
     file.close()
     return pushups_data.get(str(user_id), 0)
 
-def get_pushup_data():
+def get_pushup_data() -> dict:
     file = open(JSON_FILE, "r") 
     pushups_data = json.load(file)
     file.close()
     return pushups_data
 
-def set_pushup_data(pushup_data):
+def set_pushup_data(pushup_data: str):
     file = open(JSON_FILE, "w")
     json.dump(pushup_data, file)
     file.close()
 
-def remove_empty_values(array: list[str]):
+def strip_whitespace(array: list[str]):
     result: list[str] = []
     for v in array:
-        if v:
-            result.append(v.trim())
+        if v.strip() != "":
+            result.append(v.strip())
     return result
 
-def get_pushups_data_from_users(users: dict):
+def extract_id_from_ping(message: str) -> int | None:
+    if message[:2] != '<@':
+        return None
+    if message[-1:] != '>':
+        return None
+    try:
+        return int(message[2:-1])
+    except:
+        return None
+
+def get_pushups_data_from_users(users: list[str]) -> dict | None:
     args_users_pushups = {}
-    for i in range(len(users)):
-        if i == 0:
-            continue
-        user_id = int(users[i][2:len(users[i])-1])
+    for user_id in users:
         args_users_pushups[user_id] = get_pushup_data_from_user(user_id)
     return args_users_pushups
 
@@ -65,38 +77,39 @@ async def on_ready():
     client.command_prefix = f"<@{client.user.id}> "
     print('client ready')
 
-
 @client.event
 async def on_message(message):
     if message.author.bot:
         return
 
-    true_message = str.replace(message.content, f"<@{client.user.id}>", "")
-    args = true_message.split(" ")
-    args = remove_empty_values(args)
+    args = strip_whitespace(message.content.split(" "))
     if len(args) == 0:
         return
-    if args[0] == "stats":
-        if len(args) > 1:
-            return await message.reply(embed=pushups_embed(get_pushups_data_from_users(args)))
-                
-        pushup_data = get_pushup_data()
-
-        await message.reply(embed=pushups_embed(pushup_data))
-
-    try:
-        pushups = int(true_message)
-    except:
+    if extract_id_from_ping(args[0]) != client.user.id:
         return
-
-    pushup_data = get_pushup_data()
     
-    total_pushups = pushup_data.get(message.author.id, 0)
-    pushup_data[str(message.author.id)] = total_pushups + pushups
+    match args[1:]:
+        case ['stats', *users] if len(users) != 0:
+            users = map(extract_id_from_ping, users)
+            await message.reply(embed=pushups_embed(get_pushups_data_from_users(users)))
+        case ['stats']:
+            await message.reply(embed=pushups_embed(get_pushups_data_from_users([str(message.author.id)])))
+        case ['log', pushups]:
+            try:
+                pushups = int(pushups)
+            except:
+                return await message.reply(f"Invalid amount of pushups: '{pushups}'")
+            pushups_data = get_pushup_data()
+            total_pushups = pushups_data.get(message.author.id, 0)
+            pushups_data[str(message.author.id)] = total_pushups + pushups
+            set_pushup_data(pushups_data)
+            await message.reply(f"Wow! <@{message.author.id}> just did {pushups} pushups!")
+        case _:
+            await message.reply(embed=help_embed())
 
-    set_pushup_data(pushup_data)
-
-    await message.reply(f"Wow! <@{message.author.id}> just did {pushups} pushups!")
+if not os.path.exists("pushups.json"):
+    with open("pushups.json", "+w") as file:
+        file.write("{}")
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(BASEDIR, '.env'))
