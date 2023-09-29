968650a341b56f77383b6d4d77731d8b4eace7f2
Mikkel Troels Kongsted <mtkongsted@gmail.com>
2023-09-29 20:20:26 +0200

    add args to stats endpoint

--- main.py
+++ main.py
@@ -25,11 +25,12 @@ def pushups_embed(users_pushups):
         embed.add_field(name=user.display_name, value=pushups, inline=False)
     return embed
 
-@client.event
-async def on_ready():
-    client.command_prefix = f"<@{client.user.id}> "
-    print('client ready')
 
+def get_pushup_data_from_user(user_id: int):
+    file = open(JSON_FILE, "r") 
+    pushups_data = json.load(file)
+    file.close()
+    return pushups_data.get(str(user_id), 0)
 
 def get_pushup_data():
     file = open(JSON_FILE, "r") 
@@ -37,15 +38,45 @@ def get_pushup_data():
     file.close()
     return pushups_data
 
+def set_pushup_data(pushup_data):
+    file = open(JSON_FILE, "w")
+    json.dump(pushup_data, file)
+    file.close()
+
+def remove_empty_values(array: list[str]):
+    result: list[str] = []
+    for v in array:
+        if v:
+            result.append(v)
+    return result
+
+
+@client.event
+async def on_ready():
+    client.command_prefix = f"<@{client.user.id}> "
+    print('client ready')
+
 
 @client.event
 async def on_message(message):
     if message.author.bot:
         return
 
-    true_message = str.replace(message.content, "<@" + str(client.user.id) + ">", "").strip()
-
-    if true_message == "stats":
+    true_message = str.replace(message.content, "<@" + str(client.user.id) + "> ", "")
+    args = true_message.split(" ")
+    args = remove_empty_values(args)
+    if not len(args):
+        return
+    if args[0] == "stats":
+        if len(args) > 1:
+            args_users_pushups = {}
+            for i in range(len(args)):
+                if i == 0:
+                    continue
+                user_id = int(args[i][2:len(args[i])-1])
+                args_users_pushups[user_id] = get_pushup_data_from_user(user_id)
+            return await message.reply(embed=pushups_embed(args_users_pushups))
+                
         pushup_data = get_pushup_data()
 
         await message.reply(embed=pushups_embed(pushup_data))
@@ -60,9 +91,7 @@ async def on_message(message):
     total_pushups = pushup_data.get(message.author.id, 0)
     pushup_data[str(message.author.id)] = total_pushups + pushups
 
-    file = open(JSON_FILE, "w")
-    json.dump(pushup_data, file)
-    file.close()
+    set_pushup_data(pushup_data)
 
     await message.reply(f"Wow! <@{message.author.id}> just did {pushups} pushups!")
 
