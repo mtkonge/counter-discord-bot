d8d5e8500aa10bcd0770941c0faa10f7d5a646a2
Mikkel Troels Kongsted <mtkongsted@gmail.com>
2023-09-29 19:35:04 +0200

    Add stats command

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ intents.members = True
 
 client = commands.Bot(intents=intents, command_prefix="!")
 
+JSON_FILE = "pushups.json"
+
 def error_embed(name, value):
     embed = discord.Embed(title="Error", colour=discord.Colour.red())
     embed.add_field(name=name, value=value)
@@ -17,47 +19,52 @@ def error_embed(name, value):
 
 def pushups_embed(users_pushups):
     embed = discord.Embed(title='Pushups', colour = discord.Colour.blue())
-    for k, v in users_pushups.items():
-        embed.add_field(name=k, value=v, inline=False)
+    for user_id, pushups in users_pushups.items():
+        user = client.get_user(int(user_id))
+
+        embed.add_field(name=user.display_name, value=pushups, inline=False)
     return embed
 
 @client.event
 async def on_ready():
-    client.command_prefix = "<@" + str(client.user.id) + "> "
+    client.command_prefix = f"<@{client.user.id}> "
     print('client ready')
+
+
+def get_pushup_data():
+    file = open(JSON_FILE, "r") 
+    pushups_data = json.load(file)
+    file.close()
+    return pushups_data
+
 
 @client.event
 async def on_message(message):
     if message.author.bot:
         return
-    true_message = str.replace(message.content,"<@" + str(client.user.id) + "> ", "")
+
+    true_message = str.replace(message.content, "<@" + str(client.user.id) + ">", "").strip()
+
+    if true_message == "stats":
+        pushup_data = get_pushup_data()
+
+        await message.reply(embed=pushups_embed(pushup_data))
 
     try:
         pushups = int(true_message)
     except:
         return
+
+    pushup_data = get_pushup_data()
     
-    file = open("pushups.json", "r") 
-    pushupsData = json.load(file)
+    total_pushups = pushup_data.get(message.author.id, 0)
+    pushup_data[str(message.author.id)] = total_pushups + pushups
+
+    file = open(JSON_FILE, "w")
+    json.dump(pushup_data, file)
     file.close()
 
-    totalPushups = pushupsData.get(message.author.id, 0)
-    pushupsData[str(message.author.id)] = totalPushups + pushups
-
-    file = open("pushups.json", "w")
-    json.dump(pushupsData, file)
-    file.close()
-
-    await message.reply("Wow! <@" + str(message.author.id) + "> just did " + str(pushups) + " pushups!")
-    
-
-@client.command()
-async def stats(ctx, *args):
-    """Shows stats of members in arguments, if blank shows all members stats"""
-    users_pushups = await count_pushups(ctx, args)
-    if len(users_pushups) == 0:
-        return await ctx.send(embed=error_embed("Invalid arguments", "Arguments doesn\'t match any users in server"))
-    return await ctx.send(embed=pushups_embed(users_pushups))
+    await message.reply(f"Wow! <@{message.author.id}> just did {pushups} pushups!")
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(BASEDIR, '.env'))
